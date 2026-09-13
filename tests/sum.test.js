a0723dfc98dc269db8946ebdad04eb93ae3f9d6e
Andrii <crossoandrey@gmail.com>
2026-09-13 18:34:57 +0300

    first commit1

---
+++ tests/sum.test.js
@@ -0,0 +1,8 @@
+// tests/sum.test.js
+const sum = require('../sum');
+test('adds 1 + 2 to equal 3', () => {
+expect(sum(1, 2)).toBe(3);
+});
+test('adds 5 + 7 to equal 12', () => {
+expect(sum(5, 7)).toBe(12);
+});
